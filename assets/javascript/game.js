808e67d4c229adf442c87a7c83ec2984e186bb69
Jeff Lawlis <jeff.lawlis@yahoo.com>
2018-10-25 13:40:06 -0500

    completed base functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,6 @@
+$(document).ready(function(){
+
+
 var targetNum = 0
 var score = 0
 var crystal1 = 0
@@ -8,12 +11,40 @@ var wins = 0
 var losses = 0
 
 function resetGame(){
-    targetNum = Math.floor(Math.random() * 200)
-    crystal1 = Math.floor(Math.random() * targetNum)
-    crystal2 = Math.floor(Math.random() * targetNum)
-    crystal3 = Math.floor(Math.random() * targetNum)
-    crystal4 = Math.floor(Math.random() * targetNum)
-    console.log(targetNum)
-    console.log("1: " + crystal1, "2: " + crystal2, "3: " + crystal3, "4: " + crystal4)
+    targetNum = Math.floor(Math.random() * 50)
+    crystal1 = Math.floor(Math.random() * 10)+1
+    crystal2 = Math.floor(Math.random() * 10)+1
+    crystal3 = Math.floor(Math.random() * 10)+1
+    crystal4 = Math.floor(Math.random() * 10)+1
+    score = 0
+    $("#desired-number").text(targetNum)
+    $(".total-score").text(score)
+    $(".crystal-1").val(crystal1)
+    $(".crystal-2").val(crystal2)
+    $(".crystal-3").val(crystal3)
+    $(".crystal-4").val(crystal4)
+}
+function checkScore(){
+    if(score > targetNum){
+        losses += 1
+        $("#losses").text("Losses: " + losses)
+        alert("You went over! Try again.")
+        resetGame()
+    } else if(score === targetNum){
+        wins += 1
+        $("#wins").text("Wins: " + wins)
+        alert("You got it!")
+
+        resetGame()
+    }
 }
 resetGame()
+$("button").on("click", function(){
+    parseInt(score)
+    score += parseInt(this.value)
+    $(".total-score").text(score)
+    checkScore()
+})
+
+
+})
