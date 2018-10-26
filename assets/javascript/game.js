63a79eea369abbe9991f53ce59b766e3837baec8
Jeff Lawlis <jeff.lawlis@yahoo.com>
2018-10-26 09:32:15 -0500

    changed win/loss alerts

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,6 +19,9 @@ function resetGame(){
     score = 0
     $("#desired-number").text("Target Score: " + targetNum)
     $(".total-score").text(score)
+    $(".total-score").css("color", "black")
+    $("#wins").css("color", "black")
+    $("#losses").css("color", "black")
     $(".crystal-1").val(crystal1)
     $(".crystal-2").val(crystal2)
     $(".crystal-3").val(crystal3)
@@ -28,13 +31,17 @@ function checkScore(){
     if(score > targetNum){
         losses += 1
         $("#losses").text("Losses: " + losses)
-        alert("You went over!\nYour score: " + score + "\nTarget Score: " + targetNum + "\n Try again.")
-        resetGame()
+        // alert("You went over!\nYour score: " + score + "\nTarget Score: " + targetNum + "\n Try again.")
+        $("#losses").css("color", "red")
+        $(".total-score").css("color", "red")
+        setTimeout(resetGame, 1000 * 2)
     } else if(score === targetNum){
         wins += 1
         $("#wins").text("Wins: " + wins)
-        alert("You got it!\nYour score: " + score + "\nTarget Score: " + targetNum + "\n See if you can do it again!")
-        resetGame()
+        // alert("You got it!\nYour score: " + score + "\nTarget Score: " + targetNum + "\n See if you can do it again!")
+        $("#wins").css("color", "green")
+        $(".total-score").css("color", "green")
+        setTimeout(resetGame, 1000 * 2)
     }
 }
 resetGame()
